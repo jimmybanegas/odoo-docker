d1e961a5f006001c8cc32ed689d7a9f06b9bb1ec
Jimmy Ramos <jimmybanegas93@gmail.com>
2020-09-15 19:44:23 -0600

    change image

--- customrun.sh
+++ customrun.sh
@@ -4,4 +4,4 @@ docker run --rm -it --name=junari-odoo-dev \
     -v junari-odoo-custom-addons:/opt/odoo/custom_addons \
     -v junari-odoo-home:/home/odoo \
     -p 8069:8069 --env-file=odoo.env \
-    junari/odoo bash
+    jimmybanegas93/odoo13 bash
