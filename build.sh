a1fbde9cc78fa9367ab1f6c620326e9dd89a963e
Jimmy Ramos <jimmybanegas93@gmail.com>
2020-09-18 17:36:00 -0600

    Change build os source

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo "Building the junari/odoo-os image..."
+echo "Building the jimmybanegas93/odoo-os image..."
 docker build -t jimmybanegas93/odoo-os -f junari-odoo-os.dockerfile --pull .
 
-echo "Building the junari/odoo image..."
+echo "Building the jimmybanegas93/odoo image..."
 docker build -t jimmybanegas93/odoo11 -f junari-odoo.dockerfile .
