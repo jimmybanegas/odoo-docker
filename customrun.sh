476722529ddd18fa028e85cc01a3fcfcf3ecac63
Jimmy Ramos <jimmybanegas93@gmail.com>
2021-07-03 10:14:21 -0600

    update image name

--- customrun.sh
+++ customrun.sh
@@ -1,7 +1,7 @@
-winpty docker run --rm -it --name=junari-odoo14-dev \
+docker run --rm -it --name=junari-odoo14-dev \
     -v junari-odoo14-data:/opt/odoo/data \
     -v junari-odoo14-vscode:/opt/odoo/.vscode \
     -v junari-odoo14-custom-addons:/opt/odoo/custom_addons \
     -v junari-odoo14-home:/home/odoo \
     -p 8069:8069 --env-file=odoo.env \
-    jimmybanegas93/odoo14 bash
+    arosoft/odoo14.0 bash
